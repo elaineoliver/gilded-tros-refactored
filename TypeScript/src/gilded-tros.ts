ba38bce21bd3f79c4c1463812cd6118fac5fcc63
Elaine Oliver <e.oliver@elsevier.com>
2024-07-09 07:58:08 +0200

    Removing commented code

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -93,14 +93,6 @@ export class GildedTros {
     }
 
     public updateQuality(): void {
-        // const newItems: Item[] = []
-
-        // this.items.forEach(item => {
-        //     newItems.push(this.updateItem(item))
-        // })
-
-        // this.items = newItems
-
         for (let i = 0; i < this.items.length; i++) {
             const item = this.items[i]
 
@@ -109,58 +101,6 @@ export class GildedTros {
             this.items[i].sellIn = newItem.sellIn
             this.items[i].quality = newItem.quality
         }
-        // this.items.reduce(itemsWithCategories)
-
-        // for (let i = 0; i < this.items.length; i++) {
-        //     if (this.items[i].name != 'Good Wine' && this.items[i].name != 'Backstage passes for Re:Factor'
-        //         && this.items[i].name != 'Backstage passes for HAXX') {
-        //         if (this.items[i].quality > 0) {
-        //             if (this.items[i].name != 'B-DAWG Keychain') {
-        //                 this.items[i].quality = this.items[i].quality - 1;
-        //             }
-        //         }
-        //     } else {
-        //         if (this.items[i].quality < 50) {
-        //             this.items[i].quality = this.items[i].quality + 1;
-
-        //             if (this.items[i].name == 'Backstage passes for Re:Factor') {
-        //                 if (this.items[i].sellIn < 11) {
-        //                     if (this.items[i].quality < 50) {
-        //                         this.items[i].quality = this.items[i].quality + 1;
-        //                     }
-        //                 }
-
-        //                 if (this.items[i].sellIn < 6) {
-        //                     if (this.items[i].quality < 50) {
-        //                         this.items[i].quality = this.items[i].quality + 1;
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //     }
-
-        //     if (this.items[i].name != 'B-DAWG Keychain') {
-        //         this.items[i].sellIn = this.items[i].sellIn - 1;
-        //     }
-
-        //     if (this.items[i].sellIn < 0) {
-        //         if (this.items[i].name != 'Good Wine') {
-        //             if (this.items[i].name != 'Backstage passes for Re:Factor' || this.items[i].name != 'Backstage passes for HAXX') {
-        //                 if (this.items[i].quality > 0) {
-        //                     if (this.items[i].name != 'B-DAWG Keychain') {
-        //                         this.items[i].quality = this.items[i].quality - 1;
-        //                     }
-        //                 }
-        //             } else {
-        //                 this.items[i].quality = this.items[i].quality - this.items[i].quality;
-        //             }
-        //         } else {
-        //             if (this.items[i].quality < 50) {
-        //                 this.items[i].quality = this.items[i].quality + 1;
-        //             }
-        //         }
-        //     }
-        // }
     }
 
 }
